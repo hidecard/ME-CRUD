0f93932a7ef7fa7fe3981bc49aa2a4dbbad90a3a
hidecard <hidecard1500@gmail.com>
2025-06-22 14:37:40 +0630

    day 2

--- server.js
+++ server.js
@@ -3,3 +3,20 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const userRoutes = require('./routes/userRoutes')
 const app = express();
+
+//connect mongoDb 
+
+mongoose.connect('mongodb://localhost:27017/me-crud',{
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+})
+.then(() => console.log('MongoDB connected')) 
+.catch((err) => console.log('MongoDB connection error: ', err)); 
+
+
+// middleware 
+
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.set('view engine', 'ejs'); 
+app.use(express.static('views')); 
+
